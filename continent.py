a3bc4ecf33a41243dd0a49479f47f5917274cab5
Thilagavathycse <50851936+Thilagavathycse@users.noreply.github.com>
2020-06-15 05:00:14 -0700

    Update continent.py

--- continent.py
+++ continent.py
@@ -1,5 +1,5 @@
 dictionary={"Spain":"Europe","japan":"Asia","India":"Asia","Italy":"Europe","Thailand":"Asia","Sudan":"Africa"}
 Desired_country=input(" Desired continent:")
 for key,value in dictionary.items():
-    if value==Desired_country:
+    if value.lower()==Desired_country.lower():
         print(key)
