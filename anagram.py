9722df31f3985081674bef32184bf51ca1d208a6
Thilagavathycse <50851936+Thilagavathycse@users.noreply.github.com>
2020-06-26 01:29:05 -0700

    Update anagram.py

--- anagram.py
+++ anagram.py
@@ -1,4 +1,6 @@
-s1,s2=str(input().split())
+s1,s2=input("enter two string separated by space").split()
+s1=s1.lower()
+s2=s2.lower()
 def anagram_check(s1,s2):
    print(bool(sorted(s1)==sorted(s2)))
 anagram_check(s1,s2)
